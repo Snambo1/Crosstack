4edd659b2781d768f867433e5b3b682db8c85843
Snambo1 <105807243+Snambo1@users.noreply.github.com>
2022-05-19 21:35:18 -0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+levelMap= [
+"                    ",
+"                    ",
+"                    ",
+"XX    XXX   XXXXX XX",
+"                    ",
+"XXXXXX      XXXXXXXX",
+"                    ",
+"                    ",
+"       P             ",
+"XX    XXX   XXXXX XX",
+"XXXXXXXXXXXXXXXXXXXX"]
+
+tileSize=64
+screenWidth=1500
+screenHeight=len(levelMap)*tileSize
