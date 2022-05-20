9e87930f965ae6e6ab89e2e8426160617d32c8d9
Snambo1 <105807243+Snambo1@users.noreply.github.com>
2022-05-19 21:30:27 -0400

    Create player.py

---
+++ player.py
@@ -0,0 +1,34 @@
+import pygame
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.image = pygame.Surface((32,64))
+        self.image.fill('red')
+        self.rect=self.image.get_rect(topleft=pos)
+        self.direction=pygame.math.Vector2(0,0)
+        self.speed=8
+        self.gravity=0.8
+        self.jumpSpeed=-4
+    def getInput(self):
+        keys=pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+        if keys[pygame.K_SPACE]:
+            self.jump()
+
+    def applyGravity(self):
+        self.direction.y +=self.gravity
+        self.rect.y +=self.direction.y
+
+
+    def jump(self):
+        self.direction.y=self.jumpSpeed
+
+    def update(self):
+        self.getInput()
+        self.rect.x +=self.direction.x*self.speed
